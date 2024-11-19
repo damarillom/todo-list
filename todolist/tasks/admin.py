fb853cfc2c27fbc9f340bbc90e893f8441a31f08
damarillom <danielamarillo1@hotmail.com>
2024-11-20 00:13:16 +0100

    Implements admin

--- todolist/tasks/admin.py
+++ todolist/tasks/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Task, Tag
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'expiration_date', 'state', 'user', 'parent_task')
+    raw_id_fields = ('user', 'parent_task')
+
+
+@admin.register(Tag)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
